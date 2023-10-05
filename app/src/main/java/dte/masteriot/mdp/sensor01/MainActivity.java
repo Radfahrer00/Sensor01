3a2868e49404f1a6e80a74c70aed14010c867f7c
Quentin Mathieu <mathieu@cip.ifi.lmu.de>
2023-10-05 16:32:42 +0200

    First

--- app/src/main/java/dte/masteriot/mdp/sensor01/MainActivity.java
+++ app/src/main/java/dte/masteriot/mdp/sensor01/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.res.ResourcesCompat;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -13,7 +14,10 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.os.Vibrator;
 import android.widget.Toast;
@@ -38,6 +42,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private int numberOfSteps;
     Vibrator vibrator;
 
+    Button bProximity;
+    TextView tvProximitySensorValue;
+    private  Sensor proximitySensor;
+    boolean proximitySensorIsActive;
+
+    ImageView myImageView;
+    ScaleAnimation scaleAnimationBigger;
+    ScaleAnimation scaleAnimationSmaller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         lightSensorIsActive = false;
         accelSensorIsActive = false;
         stepSensorIsActive = false;
+        proximitySensorIsActive = false;
 
         // Get the references to the UI:
         bLight = findViewById(R.id.bLight); // button to start/stop light sensor's readings
@@ -57,6 +71,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         bStep = findViewById(R.id.bStep);
         tvStepSensorValue = findViewById(R.id.xStepMeasurement);
 
+        bProximity = findViewById(R.id.bProximity);
+        tvProximitySensorValue = findViewById(R.id.xProximityMeasurement);
+
         // Get the reference to the sensor manager and the sensor:
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         // Obtain the reference to the default light sensor of the device:
@@ -66,6 +83,35 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // Obtain the reference to the default step sensor of the device:
         stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
         vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        // Obtain the reference to the default proximity sensor of the device:
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        // Get the picture
+        myImageView = findViewById(R.id.myImageView);
+        myImageView.setImageResource(R.drawable.tchaka_pic);
+        scaleAnimationBigger = new ScaleAnimation(
+                1.0f,  // Start scale X
+                2.0f,  // End scale X
+                1.0f,  // Start scale Y
+                2.0f,  // End scale Y
+                Animation.RELATIVE_TO_SELF,  // Pivot X type
+                0.5f,  // Pivot X value (center)
+                Animation.RELATIVE_TO_SELF,  // Pivot Y type
+                0.5f   // Pivot Y value (center)
+        );
+        scaleAnimationBigger.setDuration(1000);
+
+        scaleAnimationSmaller = new ScaleAnimation(
+                1.0f,  // Start scale X
+                0.5f,  // End scale X
+                1.0f,  // Start scale Y
+                0.5f,  // End scale Y
+                Animation.RELATIVE_TO_SELF,  // Pivot X type
+                0.5f,  // Pivot X value (center)
+                Animation.RELATIVE_TO_SELF,  // Pivot Y type
+                0.5f   // Pivot Y value (center)
+        );
+        scaleAnimationSmaller.setDuration(1000);
 
 
         // Listener for the light button:
@@ -112,6 +158,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     bAccelerometer.setBackground(button_off);
                     tvAccelSensorValue.setText("Accel sensor is OFF");
                     accelSensorIsActive = false;
+                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
                 } else {
                     // register listener and make the appropriate changes in the UI:
                     sensorManager.registerListener(MainActivity.this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
@@ -151,6 +198,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 }
             }
         });
+
+        // Listener for the Proximity button:
+        bProximity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (proximitySensorIsActive) {
+                    // unregister listener and make the appropriate changes in the UI:
+                    sensorManager.unregisterListener(MainActivity.this, proximitySensor);
+                    bProximity.setText(R.string.prox_sensor_off);
+                    Drawable button_off =
+                            ResourcesCompat.getDrawable(getResources(), R.drawable.round_button_off, null);
+                    bProximity.setBackground(button_off);
+                    tvProximitySensorValue.setText("Proximity sensor is OFF");
+                    proximitySensorIsActive = false;
+                } else {
+                    // register listener and make the appropriate changes in the UI:
+                    sensorManager.registerListener(MainActivity.this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    bProximity.setText(R.string.prox_sensor_on);
+                    Drawable button_on =
+                            ResourcesCompat.getDrawable(getResources(), R.drawable.round_button_on, null);
+                    bProximity.setBackground(button_on);
+                    tvProximitySensorValue.setText("Waiting for first accel sensor value");
+                    proximitySensorIsActive = true;
+                }
+            }
+        });
     }
 
     // Methods related to the SensorEventListener interface:
@@ -167,12 +240,39 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         else if (sensorType == Sensor.TYPE_ACCELEROMETER) {
             tvAccelSensorValue.setText("X value: " + sensorEvent.values[0] + "\n" +
             "Y value: " + sensorEvent.values[1] + "\n" + "Z value: " + sensorEvent.values[2]);
+
+            // Change color of Screen according to orientation
+            float xvalue = Math.abs(sensorEvent.values[0]);
+            float yvalue = Math.abs(sensorEvent.values[1]);
+            float zvalue = Math.abs(sensorEvent.values[2]);
+
+            if (xvalue > yvalue && xvalue > zvalue) {
+                getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+            }
+            else if (yvalue > xvalue && yvalue > zvalue) {
+                getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+            }
+            else {
+                getWindow().getDecorView().setBackgroundColor(Color.RED);
+            }
+
         }
         else if (sensorType == Sensor.TYPE_STEP_DETECTOR) {
             numberOfSteps++;
             tvStepSensorValue.setText(Integer.toString(numberOfSteps));
             if (numberOfSteps % 20 == 0) {
                 vibrator.vibrate(1000);
+            }
+        }
+        else if (sensorType == Sensor.TYPE_PROXIMITY) {
+            float proximityValue = sensorEvent.values[0];
+            if (proximityValue == 0) {
+                tvProximitySensorValue.setText("Something is near");
+                myImageView.startAnimation(scaleAnimationSmaller);
+            }
+            else {
+                tvProximitySensorValue.setText("Nothing is close to the phone.");
+                myImageView.startAnimation(scaleAnimationBigger);
             }
         }
     }
@@ -188,11 +288,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // React to orientation changes here
         SharedPreferences sharedPreferences = getSharedPreferences("SensorPreferences", Context.MODE_PRIVATE);
         boolean isSensorEnabled = sharedPreferences.getBoolean("isSensorEnabled", false);
-
-        if (isSensorEnabled) {
-            // Re-enable the sensor
-        }
-
     }
 
 }
